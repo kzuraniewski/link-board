2a33e9199b768ea97a009c1b73c161b5e832505c
nick-here <zuraniewski.karol@gmail.com>
2021-08-17 23:13:35 +0200

    initial configuration

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+// @ts-nocheck
+const path = require('path');
+const Html = require('html-webpack-plugin');
+const MiniCSS = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        contentBase: path.join(__dirname, `public`),
+        publicPath: '/public/',
+        compress: true,
+        port: 9000,
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ['style-loader', MiniCSS.loader, 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new Html({
+            filename: 'index.html',
+            template: './public/index.html',
+        }),
+        new MiniCSS({
+            filename: 'style.css',
+        }),
+    ],
+};
