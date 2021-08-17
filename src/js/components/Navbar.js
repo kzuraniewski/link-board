256afd7041ebfb93f248fb36b83e99b5214262ac
nick-here <zuraniewski.karol@gmail.com>
2021-08-18 00:12:35 +0200

    ignore dist + initial dom

---
+++ src/js/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Navbar() {
+    return (
+        <nav className='navbar'>
+            <a href='#' className='navbar__link'>
+                Get started
+            </a>
+            <a href='#' className='navbar__link'>
+                Your board
+            </a>
+            <a href='#' className='navbar__link'>
+                Info
+            </a>
+        </nav>
+    );
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
 import '../scss/main.scss';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Navbar from './components/Navbar';
+
+function App() {
+    return <Navbar />;
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
