34b1ec2995558c214bae6967e6ed1c2687978b17
nick-here <zuraniewski.karol@gmail.com>
2021-08-18 00:17:51 +0200

    content component

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,15 @@ import '../scss/main.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from './components/Navbar';
+import Content from './components/Content';
 
 function App() {
-    return <Navbar />;
+    return (
+        <>
+            <Navbar />
+            <Content />
+        </>
+    );
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
