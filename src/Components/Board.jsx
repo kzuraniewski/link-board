6521f0526265ea0fba36e136a0ca70c2dc64f781
nick-here <zuraniewski.karol@gmail.com>
2021-08-23 19:00:31 +0200

    board groups initial

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import { MDBCollapse } from 'mdb-react-ui-kit';
+import React, { useState } from 'react';
 import Tile from './Tile';
+
+function BoardGroup({ name, children = null }) {
+    const [show, setShow] = useState(false);
+
+    return (
+        <div>
+            <div
+                className='d-flex justify-content-between'
+                onClick={() => setShow(show => !show)}
+                style={{ cursor: 'pointer' }}
+            >
+                <h6>{name}</h6>
+                <i className={`fas fa-sort-down${show ? '' : ' hidden'}`}></i>
+            </div>
+            <MDBCollapse show={show}>{children}</MDBCollapse>
+        </div>
+    );
+}
 
 export default function Board() {
     return (
-        <div className='bg-dark-50'>
-            <Tile title='Lorem ipsum' link='https://google.com' />
+        <div className='bg-light shadow-1-strong rounded p-4 my-3'>
+            <BoardGroup name='Group 1'>
+                <Tile size='l' title='Lorem ipsum' link='https://google.com' />
+            </BoardGroup>
+            <BoardGroup name='Group 2'></BoardGroup>
         </div>
     );
 }
