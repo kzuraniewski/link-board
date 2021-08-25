5714370fbb87898ca76a67e36552f0aafeef9f5e
nick-here <zuraniewski.karol@gmail.com>
2021-08-25 23:46:02 +0200

    Intro styling moved to scss

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -6,50 +6,46 @@ import Logo from './Logo';
 
 export default function Intro() {
     return (
-        <>
-            <MDBContainer className='p-5 mt-5'>
-                <MDBRow className='flex-wrap-reverse'>
-                    <MDBCol size='md' className='d-flex flex-column justify-content-center'>
-                        <h1 className='text-dark'>
-                            Too many bookmarks? <Logo /> is for you!
-                        </h1>
-                        <p>
-                            Clean up your bookmarks bar by moving them to <Logo small />.
-                        </p>
-                        {/* <a href="#/getstarted" className="btn btn-primary">Get started</a> */}
-                        <MDBBtn href='#/getstarted' className='mt-3 ms-0 me-auto'>
-                            Get started
-                        </MDBBtn>
-                    </MDBCol>
-                    <MDBCol
-                        size='md'
-                        className='d-flex flex-column align-items-center justify-content-center'
-                    >
-                        <img src={'badBookmarks'} alt='Bad bookmarks' className='img-fluid' />
-                        <h3>
-                            <i className='fas fa-long-arrow-alt-down mt-3 mb-3'></i>
-                        </h3>
-                        <img src={'goodBookmarks'} alt='Good bookmarks' className='img-fluid' />
-                    </MDBCol>
-                </MDBRow>
-            </MDBContainer>
-
-            <div className='p-5  bg-primary text-light'>
+        <article className='article'>
+            <div className='article__group'>
                 <MDBContainer>
-                    <MDBRow className='text-light'>
-                        <MDBCol size='md' className='d-flex flex-column justify-content-center'>
-                            <img src='asd' alt='Board preview' className='img-fluid' />
+                    <MDBRow className='article__row'>
+                        <MDBCol size='md' className='article__col'>
+                            <h1 className='article__big'>
+                                Too many bookmarks? <Logo /> is for you!
+                            </h1>
+                            <p>
+                                Clean up your bookmarks bar by moving them to <Logo small />.
+                            </p>
+                            <MDBBtn href='#/getstarted' className='article__cta'>
+                                Get started
+                            </MDBBtn>
                         </MDBCol>
-                        <MDBCol
-                            size='md'
-                            className='d-flex flex-column justify-content-center align-items-stretch'
-                        >
-                            <h1 className='text-light'>Store your links in one place</h1>
+                        <MDBCol size='md' className='article__col'>
+                            <img src={'badBookmarks'} alt='Bad bookmarks' className='article__image' />
+
+                            <i className='fas fa-long-arrow-alt-down article__arrow'></i>
+
+                            <img src={'goodBookmarks'} alt='Good bookmarks' className='article__image' />
+                        </MDBCol>
+                    </MDBRow>
+                </MDBContainer>
+            </div>
+
+            <div className='article__group'>
+                <MDBContainer>
+                    <MDBRow>
+                        <MDBCol size='md' className='article__content-container'>
+                            <img src='asd' alt='Board preview' className='article__image' />
+                        </MDBCol>
+
+                        <MDBCol size='md' className='article__content-container'>
+                            <h1 className='article__big'>Store your links in one place</h1>
                             <p>Gain access to your bookmarks from any browser or device.</p>
                         </MDBCol>
                     </MDBRow>
                 </MDBContainer>
             </div>
-        </>
+        </article>
     );
 }
