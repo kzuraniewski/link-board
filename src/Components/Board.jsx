a8f91fc61542f6ecc7f8ef19f5dd6910575f8640
nick-here <zuraniewski.karol@gmail.com>
2021-08-25 10:56:14 +0200

    tiles as block links

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -2,7 +2,7 @@ import { MDBCol, MDBCollapse, MDBContainer, MDBRow } from 'mdb-react-ui-kit';
 import React, { useState } from 'react';
 import placeholder from '../images/placeholder.jpeg';
 
-function Tile({ position, size, title, link }) {
+function Tile({ title, link }) {
     const [editMode, setEditMode] = useState(false);
     const [editable, setEditable] = useState(false);
 
@@ -10,16 +10,16 @@ function Tile({ position, size, title, link }) {
     const style = {
         backgroundImage: `url(${placeholder})`,
         backgroundSize: 'cover',
-
+        cursor: 'default'
     };
 
     return (
-        <div
-            className='text-light overflow-hidden rounded m-1 d-inline-block'
+        <a
+            className='tile d-block text-light overflow-hidden rounded m-1 d-inline-block'
+            href={link}
             style={style}
             onMouseEnter={() => setEditable(true)}
             onMouseLeave={() => setEditable(false)}
-            onClick={() => (location.href = link)}
         >
             <div className='tile-mask w-100 h-100 d-flex flex-column justify-content-end p-2 position-relative'>
                 {editable && (
@@ -27,6 +27,8 @@ function Tile({ position, size, title, link }) {
                         className='btn-edit text-light bg-transparent border-0 position-absolute top-0 end-0 mt-2 me-2'
                         onClick={e => {
                             e.preventDefault();
+                            e.stopPropagation();
+
                             setEditMode(editMode => !editMode);
                         }}
                     >
@@ -37,7 +39,7 @@ function Tile({ position, size, title, link }) {
                 <h5 className='text-truncate user-select-none'>{title}</h5>
                 <p className='tile-link text-truncate user-select-none mb-0'>{link}</p>
             </div>
-        </div>
+        </a>
     );
 }
 
@@ -66,11 +68,11 @@ export default function Board() {
     return (
         <MDBContainer className='bg-light text-dark shadow-2-strong rounded p-4 my-3'>
             <BoardGroup name='Group 1'>
-                <Tile position={[1, 1]} size='l' title='Lorem ipsum' link='https://google.com' />
-                <Tile position={[2, 2]} size='s' title='Lorem ipsum' link='https://google.com' />
+                <Tile title='Lorem ipsum' link='https://google.com' />
+                <Tile title='Lorem ipsum' link='https://google.com' />
             </BoardGroup>
             <BoardGroup name='Group 2'>
-                <Tile position={[1, 1]} size='s' title='Lorem ipsum' link='https://google.com' />
+                <Tile title='Lorem ipsum' link='https://google.com' />
             </BoardGroup>
         </MDBContainer>
     );
