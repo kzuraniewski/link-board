0e460942ff14679d21f177d6b5ab5eb332cde837
nick-here <zuraniewski.karol@gmail.com>
2021-08-25 17:18:59 +0200

    new group button + group open state from api and styling

--- src/Components/board/Board.jsx
+++ src/Components/board/Board.jsx
@@ -3,14 +3,28 @@ import React, { useEffect, useState } from 'react';
 import { BoardGroup } from './BoardGroup';
 import { Tile } from './Tile';
 
+function AddGroup({ onAdd }) {
+    return (
+        <button
+            className='p-2 btn-blank d-flex align-items-center justify-content-center text-light bg-secondary add-group rounded w-100'
+            onClick={onAdd}
+        >
+            <i className='fas fa-plus'></i>
+        </button>
+    );
+}
+
 export default function Board() {
     const [groups, setGroups] = useState([]);
+
+    const addGroup = () => {};
 
     //test
     useEffect(() => {
         setGroups([
             {
                 name: 'General',
+                open: true,
                 tiles: [
                     {
                         title: 'Lorem',
@@ -28,30 +42,28 @@ export default function Board() {
             },
             {
                 name: 'Group test',
+                open: false,
                 tiles: [
                     {
                         title: 'Sit',
-                        link: 'https://googlr.com',
+                        link: 'https://google.com',
                     },
                 ],
             },
         ]);
     }, []);
 
-    useEffect(() => {
-        console.log(groups);
-    }, [groups]);
-
     return (
         <MDBContainer className='py-4'>
             {/* translate groups into components */}
-            {groups.map(({ name, tiles }, index) => (
-                <BoardGroup key={index} name={name}>
+            {groups.map(({ name, open, tiles }, index) => (
+                <BoardGroup key={index} name={name} open={open}>
                     {tiles.map(({ title, link }, index) => (
                         <Tile key={index} title={title} link={link} />
                     ))}
                 </BoardGroup>
             ))}
+            <AddGroup onAdd={addGroup} />
         </MDBContainer>
     );
 }
