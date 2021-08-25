6e39fca4cad31c238da4b48b55d043087e526571
nick-here <zuraniewski.karol@gmail.com>
2021-08-25 17:19:13 +0200

    .

--- src/Components/board/BoardGroup.jsx
+++ src/Components/board/BoardGroup.jsx
@@ -1,27 +1,25 @@
 import { MDBCollapse } from 'mdb-react-ui-kit';
 import React, { useEffect, useState } from 'react';
 
-export function BoardGroup({ name, children = null }) {
-    const [show, setShow] = useState(false);
+export function BoardGroup({ name, open, children = null }) {
+    const [show, setShow] = useState(open);
 
     return (
         <div className={`mb-3 p-1 group rounded text-light`}>
             <div
-                className='btn-blank w-100 p-1 d-flex justify-content-between user-select-none pointer'
+                className='w-100 p-1 d-flex justify-content-between user-select-none pointer'
                 onClick={() => setShow(show => !show)}
             >
                 <h6 className='my-auto'>{name}</h6>
                 <div className='d-flex'>
                     <button
-                        className='btn-blank text-light'
+                        className='btn-blank text-light me-5'
                         onClick={e => {
-                            e.stopPropagation();
+                            if (show) e.stopPropagation();
                         }}
                     >
                         <i
-                            className={`fas fa-plus group__add me-5${
-                                show ? '' : ' group__add--hidden'
-                            }`}
+                            className={`fas fa-plus group__add${show ? '' : ' group__add--hidden'}`}
                         ></i>
                     </button>
                     <i
