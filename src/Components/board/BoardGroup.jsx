1a7d9ab7051a95d1e19724e9bf15cb41208d6056
nick-here <zuraniewski.karol@gmail.com>
2021-08-27 17:37:02 +0200

    board group name as EditableProperty

--- src/Components/board/BoardGroup.jsx
+++ src/Components/board/BoardGroup.jsx
@@ -3,14 +3,27 @@ import React, { useState } from 'react';
 import { AddTileBtn } from './AddTileBtn';
 import { CollapseArrow } from './CollapseArrow';
 import { GroupName } from './GroupName';
+import { EditableProperty } from './EditableProperty';
 
 export function BoardGroup({ name, setName, open, children = null }) {
     const [show, setShow] = useState(open);
+    const [editMode, setEditMode] = useState(name.length ? false : true);
 
     return (
         <div className='board-group'>
             <div className='board-group__topbar' onClick={() => setShow(show => !show)}>
-                <GroupName name={name} setName={setName} />
+                <EditableProperty
+                    editMode={editMode}
+                    value={name}
+                    defaultValue='New group'
+                    className='board-group__name'
+                    focus
+                    exitOnBlur
+                    onPropertySet={nameValue => {
+                        setEditMode(false);
+                        setName(nameValue);
+                    }}
+                />
 
                 <CollapseArrow show={show} />
             </div>
--- src/Components/board/EditableProperty.jsx
+++ src/Components/board/EditableProperty.jsx
@@ -4,34 +4,44 @@ import React, { useState, useEffect, useRef } from 'react';
  * Returns an input which allows to be editable when in edit mode
  * @param {object} props
  * @param {boolean} props.editMode - whether the input should be editable or not
- * @param {function} props.onPropertySet - called after entering input ended
+ * @param {function} [props.onPropertySet] - called after entering input ended
  * @param {string} [props.className = null] - optional class
  * @param {string} [props.value = ''] - initial value
+ * @param {string} [props.defaultValue] - value used instead of empty string
  * @param {boolean} [props.focus] - whether the property should be focused and selected on edit mode
+ * @param {boolean} [props.exitOnBlur] - whether to call onPropertySet on blur
+ * @param {number} [props.maxDigits] - maximal length of input value
  */
 export function EditableProperty({
-    className = null,
-    value = '',
     editMode,
-    onPropertySet,
+    value = '',
+    defaultValue = '',
+    className = null,
     focus = false,
+    exitOnBlur = false,
+    onPropertySet = null,
+    maxDigits = 999,
 }) {
     const [propertyValue, setPropertyValue] = useState(value);
     const element = useRef(null);
 
-    const maxDigits = 12;
-
+    // clear all text selection
     const clearSelection = () => {
         if (window.getSelection()) window.getSelection().removeAllRanges();
     };
 
+    // call onPropertySet if given and use default value if specified
+    const exit = () => {
+        onPropertySet?.(propertyValue.length ? propertyValue : defaultValue);
+    };
+
+    // focus on input if in edit mode, otherwise clear selection
     useEffect(() => {
         if (!editMode) return;
 
         if (focus) {
             element.current.focus();
             element.current.select();
-            console.log('focus');
         } else {
             clearSelection();
         }
@@ -45,6 +55,7 @@ export function EditableProperty({
             className={className}
             value={propertyValue}
             disabled={!editMode}
+            onClick={e => e.stopPropagation()}
             onChange={e => {
                 const value = e.target.value;
                 if (value.length <= maxDigits) setPropertyValue(value);
@@ -53,8 +64,11 @@ export function EditableProperty({
                 if (e.key === 'Enter') {
                     // @ts-ignore
                     e.target.blur();
-                    onPropertySet(propertyValue);
+                    exit();
                 }
+            }}
+            onBlur={() => {
+                if (exitOnBlur) exit();
             }}
         />
     );
