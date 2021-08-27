6a3f710a3dda8be8e846c2e5a3368b0fef180c97
nick-here <zuraniewski.karol@gmail.com>
2021-08-27 13:54:50 +0200

    Tiles editable

--- src/Components/board/Tile.jsx
+++ src/Components/board/Tile.jsx
@@ -1,11 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import placeholder from '../../images/placeholder.jpeg';
 
-export function Tile({ title, link }) {
+export function Tile({ title, link, setTileData }) {
     const [editMode, setEditMode] = useState(false);
-    const [editable, setEditable] = useState(false);
+    const [showEditBtn, setShowEditBtn] = useState(false);
 
-    // dynamic bg image and size
+    const [titleValue, setTitleValue] = useState(title);
+    const titleInput = useRef(null);
+
+    const [linkValue, setLinkValue] = useState(link);
+    const linkInput = useRef(null);
+
+    const clearSelection = () => {
+        if (window.getSelection()) window.getSelection().removeAllRanges();
+    };
+
+    useEffect(() => {
+        // focus and select title if in edit mode, otherwise deselect
+        if (editMode) {
+            titleInput.current.focus();
+            titleInput.current.select();
+        } else {
+            clearSelection();
+        }
+    }, [editMode]);
+
+    // dynamic bg image
     const style = {
         backgroundImage: `url(${placeholder})`,
     };
@@ -16,11 +36,16 @@ export function Tile({ title, link }) {
             href={link}
             target='_blank'
             style={style}
-            onMouseEnter={() => setEditable(true)}
-            onMouseLeave={() => setEditable(false)}
+            onMouseEnter={() => setShowEditBtn(true)}
+            onMouseLeave={() => setShowEditBtn(false)}
+            onClick={e => {
+                // disable link when in edit mode
+                if (editMode) e.preventDefault();
+            }}
         >
             <div className='tile__mask'>
-                {editable && (
+                {/* Edit mode button */}
+                {(editMode || showEditBtn) && (
                     <button
                         className='tile__btn-edit'
                         onClick={e => {
@@ -30,12 +55,49 @@ export function Tile({ title, link }) {
                             setEditMode(editMode => !editMode);
                         }}
                     >
-                        <i className='fas fa-pen'></i>
+                        {editMode ? (
+                            <i className='fas fa-check'></i>
+                        ) : (
+                            <i className='fas fa-pen'></i>
+                        )}
                     </button>
                 )}
 
-                <h5 className='tile__title'>{title}</h5>
-                <p className='tile__link'>{link}</p>
+                {/* name preview */}
+                <input
+                    ref={titleInput}
+                    type='text'
+                    className='tile__title'
+                    value={titleValue}
+                    disabled={!editMode}
+                    onChange={e => setTitleValue(e.target.value)}
+                    onKeyDown={e => {
+                        if (e.key === 'Enter') {
+                            // @ts-ignore
+                            e.target.blur();
+                            setEditMode(false);
+                            setTileData({ title: titleValue, link });
+                        }
+                    }}
+                />
+
+                {/* link path */}
+                <input
+                    ref={linkInput}
+                    type='text'
+                    className='tile__link'
+                    value={linkValue}
+                    disabled={!editMode}
+                    onChange={e => setLinkValue(e.target.value)}
+                    onKeyDown={e => {
+                        if (e.key === 'Enter') {
+                            // @ts-ignore
+                            e.target.blur();
+                            setEditMode(false);
+                            setTileData({ title, link: linkValue });
+                        }
+                    }}
+                />
             </div>
         </a>
     );
