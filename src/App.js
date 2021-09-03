3df8f1ca42b23ed6e3adb3060c42ac7880e1bed5
nick-here <zuraniewski.karol@gmail.com>
2021-09-03 07:11:10 +0200

    get started page removed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './Components/page/Header';
 import Board from './Components/board/Board';
-import GetStarted from './Components/page/GetStarted';
 import About from './Components/page/About';
 import Intro from './Components/page/Intro';
 import NotFound from './Components/page/NotFound';
@@ -16,7 +15,6 @@ function App() {
                     <Switch>
                         <Route exact path='/' component={Intro}></Route>
                         <Route exact path='/board' component={Board}></Route>
-                        <Route path='/getstarted' component={GetStarted}></Route>
                         <Route path='/info' component={About}></Route>
                         <Route component={NotFound}></Route>
                     </Switch>
