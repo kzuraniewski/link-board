dc5157855b16999b82ef2e36d525ccec928b2114
nick-here <zuraniewski.karol@gmail.com>
2022-04-30 18:34:48 +0200

    DynamicCollapse interface

--- src/Components/DynamicCollapse.tsx
+++ src/Components/DynamicCollapse.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
+export interface DynamicCollapseProps {
+	/**
+	 * Whether the collapse should be expanded
+	 */
+	show: boolean;
+
+	children: React.ReactNode;
+}
+
 /**
  * Collapse that adjusts its size to dynamically changing content
- * @param {object} props
- * @param {boolean} props.show - whether the collapse should be expanded
- * @param {*} [props.children]
  */
-export default function DynamicCollapse({ show, children }) {
+export default function DynamicCollapse({ show, children }: DynamicCollapseProps) {
 	return <div className={`dynamic-collapse${show ? ' show' : ''}`}>{children}</div>;
 }
