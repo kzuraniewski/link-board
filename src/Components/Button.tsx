e43eea9a4ed5bb14cd7f63f87c2d5aa75465b3cf
nick-here <zuraniewski.karol@gmail.com>
2022-04-30 18:28:53 +0200

    Button interface

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 
-export default function Button({ block = false, onClick, className = null, children }) {
+export interface ButtonProps {
+	block?: boolean;
+	onClick: React.MouseEventHandler;
+	className?: string | null;
+	children: React.ReactNode;
+}
+
+export default function Button({
+	block = false,
+	onClick,
+	className = null,
+	children,
+}: ButtonProps) {
 	return (
 		<button
 			className={`button ${block ? ' block' : ''}${className ? '' + className : ''}`}
