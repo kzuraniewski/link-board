3d9fcd8bb5cc6b3408232bc684d0bdc81e797a48
nick-here <zuraniewski.karol@gmail.com>
2022-05-01 09:50:28 +0200

    UserInfo using bootstrap

--- src/Components/authentication/UserInfo.tsx
+++ src/Components/authentication/UserInfo.tsx
@@ -1,12 +1,5 @@
-import React, { useEffect } from 'react';
-import {
-	MDBDropdownItem,
-	MDBDropdownToggle,
-	MDBDropdownMenu,
-	MDBDropdown,
-	MDBDropdownHeader,
-	MDBDropdownLink,
-} from 'mdb-react-ui-kit';
+import React from 'react';
+import { Dropdown } from 'react-bootstrap';
 import { auth, expectSignIn } from '../../firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import LogIn from './LogIn';
@@ -22,24 +15,20 @@ export default function UserInfo() {
 	}
 
 	return (
-		<MDBDropdown className="user-info">
-			<MDBDropdownToggle tag="a" className="user-info__toggle">
+		<Dropdown className="user-info">
+			<Dropdown.Toggle as="a" className="user-info__toggle">
 				<img
 					src={user.photoURL as string | undefined}
 					alt="User photo"
 					className="user-info__photo"
 				/>
-			</MDBDropdownToggle>
-			<MDBDropdownMenu className="user-info__dropdown">
-				<MDBDropdownItem>
-					<MDBDropdownHeader>{user.displayName}</MDBDropdownHeader>
-				</MDBDropdownItem>
-				<MDBDropdownItem>
-					<MDBDropdownLink onClick={handleSignOut} className="user-info__link">
-						Log out
-					</MDBDropdownLink>
-				</MDBDropdownItem>
-			</MDBDropdownMenu>
-		</MDBDropdown>
+			</Dropdown.Toggle>
+			<Dropdown.Menu className="user-info__dropdown">
+				<Dropdown.Header>{user.displayName}</Dropdown.Header>
+				<Dropdown.Item onClick={handleSignOut} className="user-info__link">
+					Log out
+				</Dropdown.Item>
+			</Dropdown.Menu>
+		</Dropdown>
 	);
 }
