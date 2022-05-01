207c4a10ea3118d466d12387a96e1266d09f43e0
nick-here <zuraniewski.karol@gmail.com>
2022-05-01 10:00:19 +0200

    About using bootstrap

--- src/Components/page/About.tsx
+++ src/Components/page/About.tsx
@@ -1,36 +1,36 @@
-import { MDBBtn, MDBCol, MDBContainer, MDBRow } from 'mdb-react-ui-kit';
+import { Container, Button, Row, Col } from 'react-bootstrap';
 import React from 'react';
 
 export default function About() {
 	return (
 		<article className="article">
 			<div className="article__group">
-				<MDBContainer>
-					<MDBRow className="article__row">
-						<MDBCol className="article__content-container">
+				<Container>
+					<Row className="article__row">
+						<Col className="article__content-container">
 							<h1>This is a prototype</h1>
 							<p>
 								LinkBoard is a web app prototype allowing you to store bookmarks in
 								grouped tiles. It uses React and Firebase.
 							</p>
-						</MDBCol>
-						<MDBCol className="article__content-container article__content-container--center">
+						</Col>
+						<Col className="article__content-container article__content-container--center">
 							<p>Project available here:</p>
 							<div>
-								<MDBBtn
+								<Button
 									size="lg"
-									floating
 									style={{
 										backgroundColor: 'rgba(0, 0, 0, 0.8)',
 									}}
 									href="https://github.com/nick-here/LinkBoard"
+									className="social-btn"
 								>
 									<i className="fab fa-github"></i>
-								</MDBBtn>
+								</Button>
 							</div>
-						</MDBCol>
-					</MDBRow>
-				</MDBContainer>
+						</Col>
+					</Row>
+				</Container>
 			</div>
 		</article>
 	);
